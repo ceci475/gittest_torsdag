710a2843dcaf461be3678b53da31f396c9f9c37b
ceci475 <ceci475p@stud.kea.dk>
2020-01-07 01:12:46 +0100

    burgermenu stylet

--- script.js
+++ script.js
@@ -40,15 +40,6 @@ function toggleMenu() {
     }
 }
 
-/* Open */
-function openNav() {
-  document.getElementById("myNav").style.height = "100%";
-}
-
-/* Close */
-function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
-}
 
 /* SLIDESHOW */
 
